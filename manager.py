dd21a1395c01329e4e48c6b2d3cf91403f78a53b
Billy Bob <billyrandombob@gmail.com>
2024-04-03 19:05:24 -0600

    Add main menu

--- manager.py
+++ manager.py
@@ -1,3 +1,48 @@
 #!/usr/bin/env python3
+import argparse
+import json
+from os import path
+from pathlib import Path
+import sys
 
-print('hello world')
+ROOT_DIR = Path(path.dirname(path.abspath(__file__))).parent
+
+def get_config(p):
+    try:
+        with open('./config/config.json', 'r') as config_file:
+            return json.load(config_file)
+    except:
+        print("Invalid config file!")
+        exit(1)
+        
+def print_menu():
+    print('Please make a selection:')
+    print('1 - Manage Posts')
+    print('2 - Manage Tags')
+    print('3 - Manage Pools')
+    print('4 - Utilities')
+    print('0 - Quit')
+    
+def main():
+    parser = argparse.ArgumentParser(description='Welcome to Szurubooru Manager')
+    parser.add_argument('-c', '--config', help='Configuration file', type=Path,
+                    default=path.join(ROOT_DIR, 'config/config.json'))
+    p = parser.parse_args(sys.argv[1:])
+    config = get_config(p)
+    print()
+    
+    selection = -1
+    while selection != '0':
+        print_menu()
+        selection = input('Selection: ')
+        print()
+        
+        if selection == '1':
+            pm.manage_posts(config)
+        elif selection == '2':
+            tm.manage_tags(config)
+        elif selection == '4':
+            um.manage_utils()
+        
+main()
+    
