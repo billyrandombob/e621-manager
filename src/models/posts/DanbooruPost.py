387be0fdd72feb674f53a8a0332db8e8ff764685
Billy Bob <billyrandombob@gmail.com>
2024-12-19 20:53:32 -0700

    Add danbooru descriptions

--- src/models/posts/DanbooruPost.py
+++ src/models/posts/DanbooruPost.py
@@ -1,6 +1,6 @@
 from src.models.posts.Post import Post
 
-class DanbooruPost(Post):
+class DanbooruPost(Post):    
     def __init__(self, file_path, metadata):
         super().__init__(file_path)
         
@@ -15,8 +15,31 @@ class DanbooruPost(Post):
             self.rating = 'u'
         elif metadata['rating'] == 'e':
             self.rating = 'e'
+            
+        if metadata['artist_commentary']:
+            self.description = self.get_description(metadata)
         
         self.tags = metadata['tag_string'].split()
         self.tags.append(self.category)
         self.prepend_prefix('dan')
         self.clean_tags()
+
+    def get_description(self, metadata):
+        description = 'h4. Artist Commentary\noriginal\n'
+        
+        if metadata['artist_commentary']['original_title']:
+            description += '[b]{0}[/b]'.format(metadata['artist_commentary']['original_title'])
+            
+        if metadata['artist_commentary']['original_description']:
+            description += '\n{0}'.format(metadata['artist_commentary']['original_description'])
+        
+        if metadata['artist_commentary']['translated_title'] or metadata['artist_commentary']['translated_description']:
+            description += '\n\nTranslated\n'
+        
+            if metadata['artist_commentary']['translated_title']:
+                description = '{0}[b]{1}[/b]'.format(description, metadata['artist_commentary']['translated_title'])
+            
+            if metadata['artist_commentary']['translated_description']:
+                description = '{0}\n{1}'.format(description, metadata['artist_commentary']['translated_description'])
+        return description
+    
